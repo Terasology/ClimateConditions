0f085c5a609ced21a5b0422314d8f81b4f1d18b9
Michael Pollind <mpollind@gmail.com>
2020-11-26 22:02:52 -0800

    feat(JOML): migrate joml structured templates
    PR: https://github.com/Terasology/StructureTemplates/pull/45

--- src/main/java/org/terasology/climateConditions/igloo/IglooRasterizer.java
+++ src/main/java/org/terasology/climateConditions/igloo/IglooRasterizer.java
@@ -3,16 +3,19 @@
 
 package org.terasology.climateConditions.igloo;
 
+import org.joml.Vector3i;
+import org.joml.Vector3ic;
 import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.entitySystem.prefab.PrefabManager;
 import org.terasology.math.ChunkMath;
-import org.terasology.math.Region3i;
+import org.terasology.math.JomlUtil;
 import org.terasology.math.geom.BaseVector3i;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
 import org.terasology.structureTemplates.components.SpawnBlockRegionsComponent;
 import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockRegion;
+import org.terasology.world.block.BlockRegionIterable;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldRasterizerPlugin;
@@ -49,16 +52,17 @@ public class IglooRasterizer implements WorldRasterizerPlugin {
 
         for (Map.Entry<BaseVector3i, Igloo> entry : structureFacet.getWorldEntries().entrySet()) {
             //Base Position is the corner position for the Igloo Structure Template.
-            Vector3i basePosition = new Vector3i(entry.getKey());
+            Vector3i basePosition = new Vector3i(JomlUtil.from(entry.getKey()));
             // Fill blocks in the required regions.
             for (SpawnBlockRegionsComponent.RegionToFill regionToFill : spawnBlockRegionsComponent.regionsToFill) {
                 Block block = regionToFill.blockType;
-                Region3i region = regionToFill.region;
-                for (Vector3i pos : region) {
+                BlockRegion region = regionToFill.region;
+                for (Vector3ic pos : BlockRegionIterable.region(region).build()) {
                     // pos is the position vector relative to the origin block of the Structural Template.
-                    pos.add(basePosition);
-                    if (chunkRegion.getRegion().encompasses(pos)) {
-                        chunk.setBlock(ChunkMath.calcRelativeBlockPos(pos), block);
+                    Vector3i p = new Vector3i(pos);
+                    p.add(basePosition);
+                    if (chunkRegion.getRegion().encompasses(JomlUtil.from(p))) {
+                        chunk.setBlock(ChunkMath.calcRelativeBlockPos(JomlUtil.from(p)), block);
                     }
                 }
             }
